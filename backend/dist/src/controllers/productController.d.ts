f894ce1ede70c208064cbfccee40e790773eb697
djemai erraid <nm_djemai@esi.dz>
2026-07-05 00:55:30 +0100

    .

--- backend/dist/src/controllers/productController.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare function createProductController(req: Request, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function getProductsController(req: Request, res: Response, next: NextFunction): Promise<void>;
-export declare function getProductController(req: Request, res: Response, next: NextFunction): Promise<void>;
-export declare function updateProductController(req: Request, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function deleteProductController(req: Request, res: Response, next: NextFunction): Promise<void>;
-export declare function orderProductController(req: Request, res: Response, next: NextFunction): Promise<void>;
-export declare function unorderProductController(req: Request, res: Response, next: NextFunction): Promise<void>;
-//# sourceMappingURL=productController.d.ts.map
